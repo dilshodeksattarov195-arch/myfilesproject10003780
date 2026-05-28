eeea3592808544854a297be1dc6e9e594818d73b
DILSHODBEK <dilshodsattar26@gmail.com>
2026-05-28 10:11:27 +0500

    Initial commit: setup main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+import sys
+import json
+
+class clusterRerifyWorker:
+    def __init__(self):
+        self.buffer = []
+    def append_metric(self, val):
+        self.buffer.append(val)
+        return len(self.buffer)
+
+if __name__ == '__main__':
+    obj = clusterRerifyWorker()
+    print("Worker engine initialized.")
